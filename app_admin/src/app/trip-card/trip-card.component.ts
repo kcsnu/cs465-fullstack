362aa548744a8adbeff0677b0233947d4e175da7
kcsnu <keir.charlton-molloy@snhu.edu>
2026-04-09 17:43:47 -0700

    Module 6 completed baseline

---
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { Trip } from '../models/trip';
+import { TripDataService } from '../services/trip-data.service';
+
+@Component({
+  selector: 'app-trip-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './trip-card.component.html',
+  styleUrl: './trip-card.component.css'
+})
+export class TripCardComponent implements OnInit {
+
+  @Input('trip') trip: any;
+
+  constructor(
+    private router: Router,
+    private tripDataService: TripDataService
+  ) {}
+
+  ngOnInit(): void {
+
+  }
+
+  public editTrip(trip: Trip) {
+    localStorage.removeItem('tripCode');
+    localStorage.setItem('tripCode', trip.code);
+    this.router.navigate(['edit-trip']);
+  }
+
+  public deleteTrip(trip: Trip) {
+    this.tripDataService.deleteTrip(trip.code)
+      .subscribe({
+        next: (value: any) => {
+          console.log(value);
+          window.location.reload();
+        },
+        error: (error: any) => {
+          console.log('Error: ' + error);
+        }
+      })
+  }
+}
